0d8a4d3f61b8f3d3804733f92c14d2beb5a15e98
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-10-01 09:03:55 +0530

    added demo page

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+	By loc = By.id("demo");
+	
+	public void getPage() {
+		System.out.println("click on demo");
+	}
+	
+	
+	
+}
